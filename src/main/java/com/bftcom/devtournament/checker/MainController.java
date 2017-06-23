8af9adb9b421dcde99e2d633200e048b8d099913
a.solonshchikov <a.solonshchikov@bftcom.com>
2017-06-23 21:51:40 +0300

    Test

--- src/main/java/com/bftcom/devtournament/checker/MainController.java
+++ src/main/java/com/bftcom/devtournament/checker/MainController.java
@@ -1,12 +1,16 @@
 package com.bftcom.devtournament.checker;
 
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -16,21 +20,47 @@ import java.util.Map;
 @Controller
 public class MainController {
 
+  private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+  @Autowired
+  public void setNamedParameterJdbcTemplate(
+      NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
+    this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
+  }
+
   @RequestMapping("/")
-  public String index(Model model) {
+  public String index() {
     return "redirect:/tasklist";
   }
 
   @RequestMapping("/tasklist")
-  public String greeting(Model model) {
-    model.addAttribute("id", 3);
+  public String taskList(Model model) {
+    String sql = "SELECT Id, InitialData FROM Task";
+    List<Task> taskList = namedParameterJdbcTemplate
+        .query(sql, (rs, rowNum) -> new Task(rs.getLong("Id"), rs.getString("InitialData")));
+    model.addAttribute("taskList", taskList);
     return "tasklist";
   }
 
-  @RequestMapping("/task")
-  public String showUser(@RequestParam("id") long id, Model model) {
+  @RequestMapping("/tasklist/{id}")
+  public String task(@PathVariable("id") long id, Model model) {
+    Map<String, Object> params = new HashMap<>();
+    params.put("id", id);
+    String sql = "SELECT Id, Description FROM Task WHERE id=:id";
+    Task task = namedParameterJdbcTemplate
+        .queryForObject(sql, params, (rs, rowNum) -> new Task(rs.getLong("Id"), rs.getString("Description")));
+    model.addAttribute("task", task);
     model.addAttribute("id", id);
     return "/task";
+  }
 
+  class Task {
+    public long id;
+    public String description;
+
+    public Task(long id, String description) {
+      this.id = id;
+      this.description = description;
+    }
   }
 }
---
+++ src/main/resources/db/sql/schema.sql
@@ -0,0 +1,9 @@
+CREATE TABLE Task (
+  Id IDENTITY,
+  Name VARCHAR(255),
+  Description CLOB,
+  InitialData CLOB,
+  Result CLOB,
+  Example_InitialData CLOB,
+  Example_Result CLOB
+)
